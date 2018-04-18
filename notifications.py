ea6b8f764c8ae62e1887b271c883647fd3a310bb
deadpool <33796817+DEADP0OL@users.noreply.github.com>
2018-04-17 22:14:13 -0500

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -1,28 +1,26 @@
-from functions import *
+from resources.functions import *
 #obtain config variables
-apitoken,url,backup,port,blockinterval,minmissedblocks,channelnames,usernames,numdelegates,blockrewards,blockspermin=getconfigs('config.json')
-message =''
-
+apitoken,url,backup,port,blockinterval,minmissedblocks,servername,channelnames,usernames,numdelegates,blockrewards,blockspermin=getconfigs('resources/config.json')
 delegatesnew=getdelegates(url)
-if message =='':
-    try:
-        delegates = pd.read_csv("delegates.csv",index_col=0)
-    except FileNotFoundError:
-        delegates=None
-        print("Counters Initialized")
-    delegates=processdelegates(delegatesnew,delegates)
-    delegates,missedblockmsglist=makemissedblockmsglist(delegates,blockinterval,minmissedblocks)
-    delegates.to_csv('delegates.csv')
-    if len(missedblockmsglist)>0:
-        slacknames=getusernames('slacknames.json')
-        userlist=getuserlist(apitoken)
-        missedblockmsglist=modifymissedblockmsglist(missedblockmsglist,slacknames,userlist)
-        message=makemissedblockmsg(missedblockmsglist,blockinterval)
-#sends the message to specified channels
-if message !='':
-    slack_client = SlackClient(apitoken)
-    channelids=getchannelids(channelnames,apitoken)
-    userids=getuserids(usernames,apitoken)
-    allchannelids=getallchannelids(channelids,userids,apitoken)
-    for channel_id in allchannelids:
-        slack_client.api_call("chat.postMessage",channel=channel_id,text=message,as_user=True)
+try:
+    delegates = pd.read_csv("files/delegates.csv",index_col=0)
+except FileNotFoundError:
+    delegates=None
+    print("Counters Initialized")
+delegates=processdelegates(delegatesnew,delegates)
+delegates,missedblockmsglist=makemissedblockmsglist(delegates,blockinterval,minmissedblocks)
+#delegates.to_csv('files/delegates.csv')
+if len(missedblockmsglist)>0:
+    client = discord.Client()
+    @client.event
+    async def on_ready():
+        discordnames=getusernames('resources/discordnames.json')
+        server = discord.utils.find(lambda m: (m.name).lower() == servername, client.servers)
+        newmissedblockmsglist=modifymissedblockmsglist(missedblockmsglist,discordnames,server)
+        message=makemissedblockmsg(newmissedblockmsglist,blockinterval)
+        for channelname in channelnames:
+            await client.send_message(getchannel(channelname,server), message)
+        for username in usernames:
+            await client.send_message(getuser(username,server), message)
+        client.close()
+    client.run(apitoken)
