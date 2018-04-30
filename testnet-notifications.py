95ad8adf5f010818ed120958658d7ed821a044eb
deadpool <33796817+DEADP0OL@users.noreply.github.com>
2018-04-29 21:42:38 -0500

    Update testnet-notifications.py

--- testnet-notifications.py
+++ testnet-notifications.py
@@ -8,21 +8,4 @@ except FileNotFoundError:
     delegates=None
     print("Counters Initialized")
 delegates=processdelegates(delegatesnew,delegates)
-#delegates,missedblockmsglist=makemissedblockmsglist(delegates,blockinterval,minmissedblocks)
 delegates.to_csv('files/testnet-delegates.csv')
-'''
-if len(missedblockmsglist)>0:
-    client = discord.Client()
-    @client.event
-    async def on_ready():
-        discordnames=getusernames('resources/testnet-discordnames.json')
-        server = discord.utils.find(lambda m: (m.name).lower() == servername, client.servers)
-        newmissedblockmsglist=modifymissedblockmsglist(missedblockmsglist,discordnames,server)
-        message=makemissedblockmsg(newmissedblockmsglist,blockinterval)
-        for channelname in channelnames:
-            await client.send_message(getchannel(channelname,server), message)
-        for username in usernames:
-            await client.send_message(getuser(username,server), message)
-        client.close()
-    client.run(apitoken)
-'''
