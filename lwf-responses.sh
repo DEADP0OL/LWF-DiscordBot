1459687552000916f5aa8547d0b99f75ea576d49
deadpool <33796817+DEADP0OL@users.noreply.github.com>
2018-05-08 14:03:03 -0500

    Update lwf-responses.sh

--- lwf-responses.sh
+++ lwf-responses.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-until ./lwf-responses.py; do
-	echo "'lwf-responses.py' crashed with exit code $?. Restarting..." >&2
+until ./lwf-discordbot.py; do
+	echo "'lwf-discordbot.py' crashed with exit code $?. Restarting..." >&2
 	sleep 1
 done
